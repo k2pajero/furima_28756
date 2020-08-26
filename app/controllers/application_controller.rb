05cd73d00eee131e5503c3473dffe77df12636ab
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-08-26 22:37:05 +0900

    軽微の記述修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,5 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :encrypted_password, :nickname, :real_family_name, :real_first_name, :reading_family_name, :reading_first_name, :birthday])
+  end
 end
