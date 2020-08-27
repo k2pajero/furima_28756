c9b32fc445bef3b7d2d6104e131adb51fd7cef7c
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-08-27 18:13:29 +0900

    バリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+   
+  validates :encrypted_password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '半角英数文字を使用してください' } 
+  validates :real_family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+  validates :real_first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+  validates :reading_family_name, presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナ文字を使用してください' }
+  validates :reading_first_name, presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナ文字を使用してください' }
+  validates :birthday, presence: true
+        
+        
 end
