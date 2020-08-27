7f33acd3d6d91148559ddc847a3a45fe4d3934ad
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-08-27 11:35:16 +0900

    index修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :encrypted_password, :nickname, :real_family_name, :real_first_name, :reading_family_name, :reading_first_name, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys:
+      [:email, :encrypted_password, :nickname,
+       :real_family_name, :real_first_name, :reading_family_name,
+       :reading_first_name, :birthday])
   end
 end
