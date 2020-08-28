7cd07b18c74c14135cfd53e8f0e4a66f3324cd5c
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-08-28 17:17:05 +0900

    FactoryBot修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,5 @@
 FactoryBot.define do
+  Faker::Config.locale ='ja'
   factory :user do
     email                   {Faker::Internet.free_email}
     password                {Faker::Internet.password(min_length: 6)}
@@ -6,8 +7,8 @@ FactoryBot.define do
     nickname                {Faker::Name.initials}
     real_family_name        {Faker::Name.last_name}
     real_first_name         {Faker::Name.first_name}
-    reading_family_name     {Faker::Name.last_name}
-    reading_first_name      {Faker::Name.first_name}
+    reading_family_name     {Gimei::Name.last.katakana}
+    reading_first_name      {Gimei::Name.first.katakana}
     birthday                {Faker::Date.birthday}
   end
 end
