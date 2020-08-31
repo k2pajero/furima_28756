c3ebcc8e310a573bd7901e2add8d4e2d1526d110
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-08-31 15:16:16 +0900

    バリデーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+
+  validates :user,             presence: true
+  validates :image,            presence: true
+  validates :name,             presence: true
+  validates :content,          presence: true
+  validates :condition_id,     presence: true
+  validates :prefection_id,    presence: true
+  validates :category_id,      presence: true
+  validates :delivery_fee_id,  presence: true
+  validates :delivery_days_id, presence: true
+  validates :price,            presence: true, format: {
+     with: /\A[0-9]+\z/, 
+     message: '半角数字を使用してください' 
+    #  ¥300~¥9,999,999未実装
+    }
 end
