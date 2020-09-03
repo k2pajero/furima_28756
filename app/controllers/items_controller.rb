d448042e70278a1a4a155c3059112547302e5040
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-09-03 17:14:00 +0900

    削除に失敗した場合の条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,8 +24,11 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if item.destroy
+      redirect_to root_path
+    else
+      redirect_to item_path(item.id)
+    end
   end
 
   private
