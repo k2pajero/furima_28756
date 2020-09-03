2d12cac2db42e516d0d2a1e519e05594fac1d43f
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-09-03 16:45:45 +0900

    destroyルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # get 'items#index'
  root "items#index"
-resources :items, only: [:index, :new, :create, :show]
+resources :items, only: [:index, :new, :create, :show, :destroy]
 resources :users, only: :create
 end
