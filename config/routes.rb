df1fc29203d587da0eb1f5ff436a6aee6b5f2ca7
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-09-03 21:46:07 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
  root "items#index"
+ get 'orders', to: 'orders#index'
 resources :items
 resources :users, only: :create
 resources :orders, only: :create
