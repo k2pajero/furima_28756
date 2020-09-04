8d3d3039a4f02ee7f204c21a3d20087bfd061cb5
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-09-04 20:34:54 +0900

    購入後の画面遷移と条件分岐

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,15 +5,22 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = OrderDelivery.create(order_params)
-    @order.save
+    @order = OrderDelivery.new(order_params)
+    if @order.valid?
+      # pay_item APIに関するprivateメソッド
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
-# 保存先はdeliveryテーブルだがここで合ってる？
+
+  # 保存先はdeliveryテーブルだがここで合ってる？
   private
   def order_params
     params.require(:order_delivery).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :tell)
   end
-# まだ作動できない
+  # まだ作動できない
   # def move_to_index
   #   # redirect_to root_path unless current_user.id != @item.user_id
   # end
