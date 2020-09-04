621fa59e61d42c43555301c55bdae4b18e37ed1c
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-09-04 20:19:55 +0900

    コントローラー編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,20 @@
 class OrdersController < ApplicationController
-  before_action :move_to_index, except: :index
+  # before_action :move_to_index, except: :index
   def index
     @item = Item.find(params[:item_id])
   end
 
   def create
+    @order = OrderDelivery.create(order_params)
+    @order.save
   end
 # 保存先はdeliveryテーブルだがここで合ってる？
   private
   def order_params
-    params.require(:order).permit(:postal_code, :prefecture_id, :city, :address, :tell, :building_name, :tell)
+    params.require(:order_delivery).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :tell)
   end
 # まだ作動できない
-  def move_to_index
-    redirect_to root_path unless current_user.id != @item.user_id
-  end
+  # def move_to_index
+  #   # redirect_to root_path unless current_user.id != @item.user_id
+  # end
 end
