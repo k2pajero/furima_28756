228ae21d859794d0bc484367c1d7f137a988ad3e
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-09-04 19:12:24 +0900

    privateメソッド

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,18 @@
 class OrdersController < ApplicationController
+  before_action :move_to_index, except: :index
   def index
     @item = Item.find(params[:item_id])
   end
 
   def create
   end
-
+# 保存先はdeliveryテーブルだがここで合ってる？
+  private
+  def order_params
+    params.require(:order).permit(:postal_code, :prefecture_id, :city, :address, :tell, :building_name)
+  end
+# まだ作動できない
+  def move_to_index
+    redirect_to root_path unless current_user.id != @item.user_id
+  end
 end
