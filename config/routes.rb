196c66cf6444a08efbb896c5ec532566ff236ee1
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-09-04 18:26:54 +0900

    ルーティングをネストにする

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
  root "items#index"
  get 'orders', to: 'orders#index'
-resources :items
+ resources :items do
+  resources :orders, only: [:index,:create]
+ end
 resources :users, only: :create
-resources :orders, only: :create
 end
