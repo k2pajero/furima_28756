36fbdfacca35e62165699d0a527301b251a85a6b
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-09-05 19:22:38 +0900

    orderコントローラー　Chargeオブジェクト　インスタンス変数　ストロングパラメータ

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,21 +5,36 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = OrderDelivery.new(order_params)
+    @item = Item.find(params[:item_id])
+    @order = OrderDelivery.new(:user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building_name, :tell, :token 
+      order_params(:user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building_name, :tell, :token))
+
     if @order.valid?
-      # pay_item APIに関するprivateメソッド
+      pay_item 
       @order.save
       return redirect_to root_path
     else
+      # 'new'?
       render 'index'
     end
   end
 
-  # 保存先はdeliveryテーブルだがここで合ってる？
   private
+  
+  # 保存先はdeliveryテーブルだがここで合ってる？
   def order_params
-    params.require(:order_delivery).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :tell)
+    params.require(:order_delivery).permit(:user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building_name, :tell, :token)
   end
+
+  def pay_item
+    Payjp.api_key = "sk_test_edced86049b529e0c33694ef"  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
+  end
+
   # まだ作動できない
   # def move_to_index
   #   # redirect_to root_path unless current_user.id != @item.user_id
