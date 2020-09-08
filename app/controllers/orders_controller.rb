29e2a845f0c41d2c33c1ab98771cffe4b410fe77
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-09-08 17:35:53 +0900

    正規表現の変数化

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,7 +9,6 @@ class OrdersController < ApplicationController
 
   def create
     @order = OrderDelivery.new(order_params)
-
     if @order.valid?
       pay_item
       @order.save
@@ -22,15 +21,17 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :tell, :token).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order).permit(
+      :postal_code, :prefecture_id, :city, :address, :building_name, :tell, :token
+    ).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
   def pay_item
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']  # PAY.JPテスト秘密鍵
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
-      amount: @item.price,           # 商品の値段
-      card: order_params[:token],    # カードトークン
-      currency: 'jpy' # 通貨の種類(日本円)
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
     )
   end
 
@@ -39,7 +40,6 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    # binding.pry
     redirect_to root_path if current_user.id == @item.user_id || !@item.order.nil?
   end
 end
--- app/form/order_delivery.rb
+++ app/form/order_delivery.rb
@@ -2,16 +2,15 @@ class OrderDelivery
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :tell, :building_name, :token, :order_id
 
+  POSTAL_CODE_REGEX = /\A[0-9]{3}[-][0-9]{4}\z/.freeze
+  TELL_NUMBER_REGEX = /\A[0-9]+\z/.freeze
+
   with_options presence: true do
     validates :prefecture_id, numericality: { other_than: 0 }
-    validates :postal_code, format: {
-      with: /\A[0-9]{3}[-][0-9]{4}\z/, messsgge: '「-」も入力してください'
-    }
+    validates :postal_code, format: { with: POSTAL_CODE_REGEX, messsgge: '「-」も入力してください' }
     validates :city
     validates :address
-    validates :tell, format: {
-      with: /\A[0-9]+\z/, message: '半角数字で入力してください。「-」は不要です'
-    },
+    validates :tell, format: { with: TELL_NUMBER_REGEX, message: '半角数字で入力してください。「-」は不要です' },
                      length: { maximum: 11 }
     validates :token, presence: true
   end
@@ -19,7 +18,8 @@ class OrderDelivery
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     Delivery.create(
-      postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, tell: tell, order_id: order.id
+      postal_code: postal_code, prefecture_id: prefecture_id, city: city,
+      address: address, building_name: building_name, tell: tell, order_id: order.id
     )
   end
 end
